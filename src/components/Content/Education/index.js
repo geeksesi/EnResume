3fe2b221a7a813eea88c248c589e0c6bf7df4d5e
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2021-01-31 18:24:14 +0330

    fix education year

--- src/components/Content/Education/index.js
+++ src/components/Content/Education/index.js
@@ -7,7 +7,7 @@ export default () => {
         <div className="Education">
             <Descriptions title="Education" column={1} layout="vertical">
                 <Descriptions.Item label="Computer enginierring">
-                    Islamic azad university at Qom,Iran (From 2019)
+                    Islamic azad university at Qom,Iran (From 2018)
                 </Descriptions.Item>
             </Descriptions>
         </div>
